8cfca8d837b846b585e0eaa8eb4d404cc2fb0f15
nbauma109 <nbauma109@github.com>
2023-09-24 19:48:02 +0200

    M3_VERSION=3.8.8

--- jitci-test.sh
+++ jitci-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
+export M3_VERSION=3.8.8
+#export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 export JAVA_HOME=$HOME/jdk-17.0.8.1+1
 ln -s "$HOME/apache-maven-${M3_VERSION}/bin/mvn" ~/bin/mvn
 export PATH=~/bin:$PATH

2d04fb7e270286eb0fd629e2cea8fdd20e65dcdb
nbauma109 <nbauma109@github.com>
2023-09-24 17:47:07 +0200

    fix sym link

--- jitci-test.sh
+++ jitci-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 export JAVA_HOME=$HOME/jdk-17.0.8.1+1
-ln -sf "$HOME/apache-maven-${M3_VERSION}/bin/mvn" /usr/local/bin/mvn
+ln -s "$HOME/apache-maven-${M3_VERSION}/bin/mvn" ~/bin/mvn
+export PATH=~/bin:$PATH
 mvn -B test --no-transfer-progress

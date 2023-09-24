6f0d4f7e8ee5de00e6363ab71d26d5cdc93e9081
nbauma109 <nbauma109@github.com>
2023-09-24 14:02:38 +0200

    update ci scripts

--- jitci-init.sh
+++ jitci-init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-export JAVA_HOME=$HOME/tools/jdk17/jdk-17.0.1
+curl -vkL https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8.1%2B1/OpenJDK17U-jdk_x64_linux_hotspot_17.0.8.1_1.tar.gz -o $HOME/OpenJDK17U-jdk_x64_linux_hotspot_17.0.8.1_1.tar.gz
+tar xzvf $HOME/OpenJDK17U-jdk_x64_linux_hotspot_17.0.8.1_1.tar.gz -C $HOME
+export JAVA_HOME=$HOME/jdk-17.0.8.1+1
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 curl -vkL https://archive.apache.org/dist/maven/maven-3/${M3_VERSION}/binaries/apache-maven-${M3_VERSION}-bin.zip -o $HOME/apache-maven-${M3_VERSION}-bin.zip
 unzip -o $HOME/apache-maven-${M3_VERSION}-bin.zip -d $HOME
--- jitci-test.sh
+++ jitci-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
-export JAVA_HOME=$HOME/tools/jdk17/jdk-17.0.1
+export JAVA_HOME=$HOME/jdk-17.0.8.1+1
 $HOME/apache-maven-${M3_VERSION}/bin/mvn -B test --no-transfer-progress

ed2fadc8ae87296810a8c27a69b0155fdf1dd22a
nbauma109 <nbauma109@github.com>
2023-09-24 13:05:48 +0200

    update ci scripts

--- jitci-init.sh
+++ jitci-init.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-curl -vkL https://github.com/ibmruntimes/semeru17-binaries/releases/download/jdk-17.0.8.1%2B1_openj9-0.40.0/ibm-semeru-open-jdk_x64_linux_17.0.8.1_1_openj9-0.40.0.tar.gz -o $HOME/ibm-semeru-open-jdk_x64_linux_17.0.8.1_1_openj9-0.40.0.tar.gz
-tar xzvf $HOME/ibm-semeru-open-jdk_x64_linux_17.0.8.1_1_openj9-0.40.0.tar.gz -C $HOME
-export JAVA_HOME=$HOME/jdk-17.0.8.1+1
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 curl -vkL https://archive.apache.org/dist/maven/maven-3/${M3_VERSION}/binaries/apache-maven-${M3_VERSION}-bin.zip -o $HOME/apache-maven-${M3_VERSION}-bin.zip
 unzip -o $HOME/apache-maven-${M3_VERSION}-bin.zip -d $HOME

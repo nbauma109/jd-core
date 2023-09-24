87c577f9cc41e59008789cf9f93d195b4e2e5433
nbauma109 <nbauma109@github.com>
2023-09-24 10:46:14 +0200

    jitci scripts

---
+++ jitci-build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
+export JAVA_HOME=$HOME/jdk-17.0.8.1+1
+$HOME/apache-maven-${M3_VERSION}/bin/mvn -B install -DskipTests -Dsigning.disabled=true 

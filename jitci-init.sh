588fbe9405bfdcfea35b6dd71e985b7f45d63d31
nbauma109 <nbauma109@github.com>
2023-09-24 13:35:21 +0200

    fix ci scripts

--- jitci-init.sh
+++ jitci-init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export JAVA_HOME=$HOME/tools/jdk17/jdk-17.0.1
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 curl -vkL https://archive.apache.org/dist/maven/maven-3/${M3_VERSION}/binaries/apache-maven-${M3_VERSION}-bin.zip -o $HOME/apache-maven-${M3_VERSION}-bin.zip
 unzip -o $HOME/apache-maven-${M3_VERSION}-bin.zip -d $HOME

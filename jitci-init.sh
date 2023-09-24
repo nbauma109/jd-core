df4b8a95b5c6a2f777f7061d26cdf77625ada75d
nbauma109 <nbauma109@github.com>
2023-09-24 10:59:45 +0200

    jitci scripts

--- jitci-init.sh
+++ jitci-init.sh
@@ -5,4 +5,4 @@ export JAVA_HOME=$HOME/jdk-17.0.8.1+1
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 curl -vkL https://archive.apache.org/dist/maven/maven-3/${M3_VERSION}/binaries/apache-maven-${M3_VERSION}-bin.zip -o $HOME/apache-maven-${M3_VERSION}-bin.zip
 unzip -o $HOME/apache-maven-${M3_VERSION}-bin.zip -d $HOME
-$HOME/apache-maven-${M3_VERSION}/bin/mvn -B de.qaware.maven:go-offline-maven-plugin:resolve-dependencies
+$HOME/apache-maven-${M3_VERSION}/bin/mvn -B de.qaware.maven:go-offline-maven-plugin:resolve-dependencies --no-transfer-progress

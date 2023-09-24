975a9ad6b33f1ec128977e4ba1621ab57b4b80d1
nbauma109 <nbauma109@github.com>
2023-09-24 17:19:20 +0200

    use mvn symbolic link

--- jitci-test.sh
+++ jitci-test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 export JAVA_HOME=$HOME/jdk-17.0.8.1+1
-alias mvn="$HOME/apache-maven-${M3_VERSION}/bin/mvn"
+ln -s "$HOME/apache-maven-${M3_VERSION}/bin/mvn" /usr/local/bin/mvn
 mvn -B test --no-transfer-progress

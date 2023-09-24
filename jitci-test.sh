e2f373388beb31aa73a4da30ad35b2caa734e314
nbauma109 <nbauma109@github.com>
2023-09-24 17:08:17 +0200

    use alias mvn

--- jitci-test.sh
+++ jitci-test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 export JAVA_HOME=$HOME/jdk-17.0.8.1+1
-export PATH=$JAVA_HOME/bin:$HOME/apache-maven-${M3_VERSION}/bin
+alias mvn="$HOME/apache-maven-${M3_VERSION}/bin/mvn"
 mvn -B test --no-transfer-progress

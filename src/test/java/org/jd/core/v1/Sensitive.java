6de19d1e966db32c5b1e356fa54fff5dbd742e12
nbauma109 <nbauma109@github.com>
2025-12-28 18:04:06 +0100

    handle record component annotations

---
+++ src/test/java/org/jd/core/v1/Sensitive.java
@@ -0,0 +1,12 @@
+package org.jd.core.v1;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER, ElementType.RECORD_COMPONENT})
+public @interface Sensitive {
+    String value();
+}
